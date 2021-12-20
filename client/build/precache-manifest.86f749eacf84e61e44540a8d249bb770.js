fbe1f4d53f225a04c11c23720adfee13a0e653e6
Maki Aboabida <maki-miko@hotmail.com>
2021-12-19 23:27:18 -0500

    Updated package.json

---
+++ client/build/precache-manifest.86f749eacf84e61e44540a8d249bb770.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e54d422a30fdb6b87e100dd8f3bd493c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fd2c687867cb4b7e18c",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "2f3ff8db602d0ab813e8",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "7fd2c687867cb4b7e18c",
+    "url": "/static/js/2.c23aef83.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.c23aef83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f3ff8db602d0ab813e8",
+    "url": "/static/js/main.a5b5b7b6.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
